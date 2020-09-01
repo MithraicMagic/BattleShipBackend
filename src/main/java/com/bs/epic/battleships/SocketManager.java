a66903d244ac6216a91cc5ad7974b43b311e1fce
MithraicMagic <nickelbink@gmail.com>
2020-09-01 21:26:27 +0200

    fwefweffwef

--- src/main/java/com/bs/epic/battleships/SocketManager.java
+++ src/main/java/com/bs/epic/battleships/SocketManager.java
@@ -12,7 +12,7 @@ public class SocketManager {
 
     public void init() {
         Configuration config = new Configuration();
-        config.setHostname("localhost");
+        config.setHostname("0.0.0.0");
         config.setPort(6003);
         config.setContext("/sockets");
 
