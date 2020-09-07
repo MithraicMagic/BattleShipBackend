31e991792fbc2454d7003fa2090db34704f44ab6
Nick Walraven <nickelbink@gmail.com>
2020-09-07 07:04:26 +0200

    - Added default constructor for DonePlacing object

--- src/main/java/com/bs/epic/battleships/events/DonePlacing.java
+++ src/main/java/com/bs/epic/battleships/events/DonePlacing.java
@@ -4,6 +4,8 @@ public class DonePlacing {
     public int lobbyId;
     public String uid;
 
+    public DonePlacing() {}
+
     public DonePlacing(int lobbyId, String uid) {
         this.lobbyId = lobbyId;
         this.uid = uid;
