58ddcd8e11cf16b084666ae00084ad80e02ef75d
Cesar Pulles <cesarpulles@gmail.com>
2020-09-13 21:39:09 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/main/java/com/bs/epic/battleships/documentation/Input.java
@@ -0,0 +1,8 @@
+package com.bs.epic.battleships.documentation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Input {
+    public Collection<Tuple> fields = new ArrayList<>();
+}
