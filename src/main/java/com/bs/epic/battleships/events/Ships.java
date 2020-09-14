e520812e678f31ea3fd6b3d82d4940468b92414f
Cesar Pulles <cesarpulles@gmail.com>
2020-09-14 09:00:43 +0200

    Added Ships class documentation

--- src/main/java/com/bs/epic/battleships/events/Ships.java
+++ src/main/java/com/bs/epic/battleships/events/Ships.java
@@ -1,10 +1,12 @@
 package com.bs.epic.battleships.events;
 
+import com.bs.epic.battleships.documentation.Doc;
 import com.bs.epic.battleships.game.Ship;
 
 import java.util.Collection;
 
 public class Ships {
+    @Doc(description = "A collection of all the ships that were placed automatically")
     public Collection<Ship> ships;
 
     public Ships(Collection<Ship> ships) {
