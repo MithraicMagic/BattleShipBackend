ad25335f7f1bd3abf30e007dca57455d22aab4e3
Cesar Pulles <cesarpulles@gmail.com>
2020-11-01 16:55:18 +0100

    Added even more BIG tests

---
+++ src/test/java/com/bs/epic/battleships/unit/sockets/OnLeaveLobby.java
@@ -0,0 +1,97 @@
+package com.bs.epic.battleships.unit.sockets;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.bs.epic.battleships.SocketEvents;
+import com.bs.epic.battleships.events.ErrorEvent;
+import com.bs.epic.battleships.events.LeaveLobby;
+import com.bs.epic.battleships.lobby.Lobby;
+import com.bs.epic.battleships.lobby.LobbyManager;
+import com.bs.epic.battleships.rest.service.MessageService;
+import com.bs.epic.battleships.user.UserManager;
+import com.bs.epic.battleships.user.player.Player;
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.SocketIOClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+public class OnLeaveLobby {
+    private SocketEvents socketEvents;
+
+    private UserManager userManager = mock(UserManager.class);
+    private LobbyManager lobbyManager = mock(LobbyManager.class);
+
+    private SocketIOClient socketOne = mock(SocketIOClient.class);
+    private SocketIOClient socketTwo = mock(SocketIOClient.class);
+
+    private MessageService messageService = mock(MessageService.class);
+
+    private AckRequest ackRequest = mock(AckRequest.class);
+
+    private LeaveLobby data = new LeaveLobby("UID", 5);
+
+    private Player playerOne = new Player("Name", socketOne, "Code");
+    private Player playerTwo = new Player("Name2", socketTwo, "Code2");
+
+    private Lobby lobby = new Lobby(5, playerOne, playerTwo);
+
+    @BeforeEach
+    public void beforeEach() {
+        socketEvents = new SocketEvents(null, userManager, lobbyManager, null, null, messageService);
+    }
+
+    @Test
+    public void testPlayerNull() {
+        var errorCaptor = ArgumentCaptor.forClass(ErrorEvent.class);
+
+        socketEvents.onLeaveLobby(socketOne, data, ackRequest);
+        verify(socketOne).sendEvent(eq("errorEvent"), errorCaptor.capture());
+
+        var error = errorCaptor.getValue();
+        assertEquals("Something went horribly wrong. Try refreshing the page.", error.reason);
+    }
+
+    @Test
+    public void testLobbyNull() {
+        var errorCaptor = ArgumentCaptor.forClass(ErrorEvent.class);
+
+        when(userManager.getUser("UID")).thenReturn(playerOne);
+        when(lobbyManager.getLobby(5)).thenReturn(null);
+
+        socketEvents.onLeaveLobby(socketOne, data, ackRequest);
+        verify(socketOne).sendEvent(eq("errorEvent"), errorCaptor.capture());
+
+        var error = errorCaptor.getValue();
+        assertEquals("You tried to leave a lobby that doesn't exist.", error.reason);
+    }
+    
+    @Test
+    public void testLeaveNonPlayerLobby() {
+        var errorCaptor = ArgumentCaptor.forClass(ErrorEvent.class);
+
+        when(userManager.getUser("UID")).thenReturn(playerOne);
+        when(lobbyManager.getLobby(5)).thenReturn(mock(Lobby.class));
+
+        socketEvents.onLeaveLobby(socketOne, data, ackRequest);
+        verify(socketOne).sendEvent(eq("errorEvent"), errorCaptor.capture());
+
+        var error = errorCaptor.getValue();
+        assertEquals("You tried leaving a lobby that you're not a part of.", error.reason);
+    }
+
+    @Test
+    public void test() {
+        when(userManager.getUser("UID")).thenReturn(playerOne);
+        when(lobbyManager.getLobby(5)).thenReturn(lobby);
+
+        socketEvents.onLeaveLobby(socketOne, data, ackRequest);
+        verify(socketOne, never()).sendEvent(eq("errorEvent"), any());
+    }
+}
---
+++ src/test/java/com/bs/epic/battleships/unit/sockets/OnPlaceShip.java
@@ -0,0 +1,79 @@
+package com.bs.epic.battleships.unit.sockets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import com.bs.epic.battleships.SocketEvents;
+import com.bs.epic.battleships.events.ErrorEvent;
+import com.bs.epic.battleships.events.PlaceShip;
+import com.bs.epic.battleships.lobby.Lobby;
+import com.bs.epic.battleships.lobby.LobbyManager;
+import com.bs.epic.battleships.rest.service.MessageService;
+import com.bs.epic.battleships.user.UserManager;
+import com.bs.epic.battleships.user.player.Player;
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.SocketIOClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+public class OnPlaceShip {
+    private SocketEvents socketEvents;
+
+    private UserManager userManager = mock(UserManager.class);
+    private LobbyManager lobbyManager = mock(LobbyManager.class);
+
+    private SocketIOClient socketOne = mock(SocketIOClient.class);
+    private SocketIOClient socketTwo = mock(SocketIOClient.class);
+
+    private MessageService messageService = mock(MessageService.class);
+
+    private AckRequest ackRequest = mock(AckRequest.class);
+
+    private Player playerOne = new Player("Player1", socketOne, "Code1");
+    private Player playerTwo = new Player("Player2", socketOne, "Code2");
+
+    private Lobby lobby = new Lobby(5, playerOne, playerTwo);
+
+    @BeforeEach
+    public void beforeEach() {
+        socketEvents = new SocketEvents(null, userManager, lobbyManager, null, null, messageService);
+        lobby.initGame(10);
+    }
+
+    @Test
+    public void testLobbyIsNull() {
+        var errorCaptor = ArgumentCaptor.forClass(ErrorEvent.class);
+        var data = new PlaceShip(5, 0, 0, "UID", "carrier", true);
+
+        socketEvents.onPlaceShip(socketOne, data, ackRequest);
+        verify(socketOne).sendEvent(eq("errorEvent"), errorCaptor.capture());
+
+        var error = errorCaptor.getValue();
+        assertEquals("Invalid lobby.", error.reason);
+    }
+
+    @Test
+    public void testPlaceOutsideGrid() {
+        var errorCaptor = ArgumentCaptor.forClass(ErrorEvent.class);
+        var data = new PlaceShip(5, 0, 0, "UID", "carrier", true);
+
+        when(lobbyManager.getLobby(5)).thenReturn(lobby);
+
+        socketEvents.onPlaceShip(socketOne, data, ackRequest);
+        verify(socketOne).sendEvent(eq("errorEvent"), errorCaptor.capture());
+
+        var error = errorCaptor.getValue();
+        assertEquals("You can't place the ship outside of your grid", error.reason);
+    }
+
+    @Test
+    public void test() {
+        var data = new PlaceShip(5, 5, 5, "UID", "carrier", true);
+
+        when(lobbyManager.getLobby(5)).thenReturn(lobby);
+
+        socketEvents.onPlaceShip(socketOne, data, ackRequest);
+        verify(socketOne, never()).sendEvent(eq("errorEvent"), any());
+    }
+}
